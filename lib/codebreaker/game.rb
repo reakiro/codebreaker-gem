b0f7dbe0b97e878627bddcf56a420a2e0d05e5a2
reakiro <platypusdarina@gmail.com>
2019-07-13 14:27:42 +0300

    removed stats

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -7,24 +7,15 @@ module Codebreaker
     include Comparing
 
     attr_reader :secret_number, :attempts_number, :hints_number
-    attr_accessor :stats
 
     def initialize(attempts_number, hints_number)
       @secret_number = secret_number_generate
       @attempts_number = attempts_number
       @hints_number = hints_number
-      @stats = {
-        name: '',
-        level: '',
-        attempts_used: 0,
-        hints_used: 0,
-        date: ''
-      }
     end
 
     def process(input_number)
       @attempts_number -= 1
-      @stats[:attempts_used] += 1
       comparing(input_number)
     end
 
@@ -36,7 +27,6 @@ module Codebreaker
       return unless @hints_number.positive?
 
       @hints_number -= 1
-      @stats[:hints_used] += 1
       @secret_number.sample
     end
 
